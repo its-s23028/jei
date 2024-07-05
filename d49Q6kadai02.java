df61c1077c3e1753d60a8d1039a6e9b8776e432c
s23028 <s23028@std.it-college.ac.jp>
2024-07-05 02:27:17 +0000

    test

---
+++ d49Q6kadai02.java
@@ -0,0 +1,83 @@
+/*
+ * 問６のプログラムをアレンジして実行例のように
+追加で中央値を求めるようにしてください
+今回も引数の数は奇数を前提にやってみましょう
+ *実行例：
+>java d49Q6kadai02 456 232 400 500 250
+平均値：367.6
+値の範囲：268
+平均より大きい値の個数：3 平均より小さい値の個数：2
+中央値：400 
+*/
+public class d49Q6 {public class d49Q6 {
+   public static void main(String[] arguments) {
+      int[] nums = new int[arguments.length];
+
+      int i = 0;
+      for (String arg : arguments) {
+          nums [i++] = Integer.parseInt(arg);
+      }
+
+      Analyzer analyzer = new Analyzer();
+      analyzer.setNums(nums);
+      System.out.println(analyzer.analyze());
+   }
+}
+class Analyzer {
+      int[] nums;
+
+      void setNums(int[] nums) {
+	 this.nums = nums;
+      }
+
+      String analyze() {
+	 StringBuilder sb = new StringBuilder();
+
+	 double avg = getAverage();
+	 sb.append("平均値:" + avg + "¥n");
+
+	 int r = getRange();
+	 sb.append("値の範囲：" + r + "¥n");
+
+	 int[] counts = getHighLowCounts(avg);
+	 sb.append("平均より大きい値の個数：" + counts[0] + "平均より小さい値の個数:" + counts[1] + "¥n");
+
+	 return sb.toString();
+       }
+
+      double getAverage() {
+	 double sum = 0;
+	 for (int n : nums) {
+            sum += n;
+	 }
+	 return sum / nums.length;
+       }
+
+      int getRange() {
+	  int min = 1000, max = -1000;
+	  for (int n : nums) {
+	     if (n < min) {
+		min = n;
+	     }
+	     if (n > max) {
+		max = n;
+	     } 
+       }
+    return max -min;
+      }
+
+      int[] getHighLowCounts(double avg) {
+	   int high = 0, low = 0;
+	   for (int n : nums) {
+	      if ( n < avg) {
+		 low++;
+	      }
+	      if (n > avg) {
+		 high++;
+	      }
+	   }
+	   return new int[]{high,low};
+    }
+
+   }
+}
