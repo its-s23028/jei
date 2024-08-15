00541c5674db3138c4fb271b4259bad0df39b713
s23028 <s23028@std.it-college.ac.jp>
2024-08-15 08:34:42 +0000

    課題

---
+++ d48/d48Q27kadai.java
@@ -0,0 +1,13 @@
+// s23028
+// d48Q27kadaiの修正
+
+public class d48Q27kadai {
+    public static void main(String[] args) {
+    int i = 0;
+	while ( i <= 6 ) {
+	System.out.print(i);
+	    i++;
+	}
+	System.out.println();
+    }
+}
