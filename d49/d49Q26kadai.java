97da87d524378bf30bcb2923fbd39d38cc6d99d3
s23028 <s23028@std.it-college.ac.jp>
2024-08-15 09:00:58 +0000

    課題26

---
+++ d49/d49Q26kadai.java
@@ -0,0 +1,14 @@
+//引数を指定するプログラム
+//ｓ２３０２８
+import java.util.Scanner;
+
+    public class d49Q26kadai {
+    public static void main(String[] args) {
+	d49Q26kadai obj = new d49Q26kadai();
+	obj.show(args[0]);
+    }
+
+    void show(String n) {
+		System.out.println("value is " + n);
+    }
+}
